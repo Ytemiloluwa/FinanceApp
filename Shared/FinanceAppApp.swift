e61c3f112a5040d5d035d7951eea82dd1ef592a6
Temi <ytemiloluwa@yahoo.com>
2021-09-28 11:26:10 +0100

    Initial Commit

---
+++ Shared/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  Shared
+//
+//  Created by Temi on 28/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
